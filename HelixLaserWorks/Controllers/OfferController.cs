53ba9474bcbe689f2aa89925d8df05246163d810
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-03-04 15:38:36 +0200

    Offer controller added

---
+++ HelixLaserWorks/Controllers/OfferController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelixLaserWorks.Controllers
+{
+    public class OfferController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
