088765e7c3067db3ab9a0a8ebad743b45d48be5b
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-03-05 20:22:25 +0200

    Materials Service added

---
+++ HelixLaserWorks.Core/Contracts/IMaterialService.cs
@@ -0,0 +1,11 @@
+﻿using HelixLaserWorks.Core.Models.Materials;
+
+namespace HelixLaserWorks.Core.Contracts
+{
+    public interface IMaterialService
+    {
+        Task<IEnumerable<MaterialViewModel>> All();
+
+        Task<MaterialDetailViewModel?> Details(int id);
+    }
+}
---
+++ HelixLaserWorks.Core/Services/MaterialService.cs
@@ -0,0 +1,56 @@
+﻿using HelixLaserWorks.Core.Contracts;
+using HelixLaserWorks.Core.Models.Materials;
+using HelixLaserWorks.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelixLaserWorks.Core.Services
+{
+    public class MaterialService : IMaterialService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MaterialService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<MaterialViewModel>> All()
+        {
+            return await _context.Materials
+                .AsNoTracking()
+                .Select(m => new MaterialViewModel()
+                {
+                    Id = m.Id,
+                    Name = m.Name,
+                    Description = m.Description,
+                    Type = m.MaterialType.Name,
+                    ImageUrl = m.ImageUrl
+                })
+                .ToListAsync();
+        }
+
+        public async Task<MaterialDetailViewModel?> Details(int id)
+        {
+            return await _context.Materials
+                .AsNoTracking()
+                .Where(m => m.Id == id)
+                .Select(m => new MaterialDetailViewModel()
+                {
+                    Name = m.Name,
+                    Description = m.Description,
+                    Type = m.MaterialType.Name,
+                    ImageUrl = m.ImageUrl,
+                    Price = m.PricePerSquareMeter.ToString(),
+                    AvailableThicknesses = string.Join(", ", m.MaterialThicknesses.Select(mt => mt.Thickness.Value)),
+                    Density = m.Density.ToString(),
+                    Rusting = m.CorrosionResistance ? "No" : "Yes"
+                })
+                .FirstOrDefaultAsync();
+        }
+    }
+}
