7d84fe6ce1b2378c0a0c3890bc928b5b7b347d18
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-03-05 20:22:52 +0200

    Material View Models added

---
+++ HelixLaserWorks.Core/Models/Materials/MaterialDetailViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace HelixLaserWorks.Core.Models.Materials
+{
+    public class MaterialDetailViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Type { get; set; } = string.Empty;
+
+        public string Price { get; set; } = string.Empty;
+
+        public string Density { get; set; } = string.Empty;
+
+        public string Rusting { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public string AvailableThicknesses { get; set; } = string.Empty;
+    }
+}
---
+++ HelixLaserWorks.Core/Models/Materials/MaterialViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace HelixLaserWorks.Core.Models.Materials
+{
+    public class MaterialViewModel
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
