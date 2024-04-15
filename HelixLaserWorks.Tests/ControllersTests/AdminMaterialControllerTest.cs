3f108525c7b9a908ad3a708afab157ff3874dd1d
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-04-15 17:02:49 +0300

    MaterialController tests added

---
+++ HelixLaserWorks.Tests/ControllersTests/AdminMaterialControllerTest.cs
@@ -0,0 +1,181 @@
+﻿using HelixLaserWorks.Areas.Admin.Controllers;
+using HelixLaserWorks.Core.Contracts;
+using HelixLaserWorks.Core.Models.Material;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace HelixLaserWorks.Tests.ControllersTests
+{
+    [TestFixture]
+    public class AdminMaterialControllerTest
+    {
+        private Mock<IMaterialService> _materialServiceMock;
+        private MaterialController _materialController;
+        private Mock<IThicknessService> _thicknessServiceMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _thicknessServiceMock = new Mock<IThicknessService>();
+            _materialServiceMock = new Mock<IMaterialService>();
+            _materialController = new MaterialController(_materialServiceMock.Object, _thicknessServiceMock.Object);
+        }
+
+        [Test] //GET
+        public async Task ActionAddShouldReturnViewResultWithCorrectModel()
+        {
+            var availableThicknesses = new List<double>() { 1, 2, 3 };
+            var types = new List<MaterialTypeViewModel>()
+            {
+                new MaterialTypeViewModel()
+                {
+                    Id = 1,
+                    Name = "Metal"
+                }
+            };
+
+            _thicknessServiceMock.Setup(ts => ts.GetAllThicknessesAsync()).ReturnsAsync(availableThicknesses);
+            _materialServiceMock.Setup(ms => ms.GetAllATypesAsync()).ReturnsAsync(types);
+
+            _materialController = new MaterialController(_materialServiceMock.Object, _thicknessServiceMock.Object);
+
+            var result = await _materialController.Add();
+
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+
+            var viewModel = result as ViewResult;
+
+            Assert.IsNotNull(viewModel);
+
+            var model = viewModel.ViewData.Model as MaterialFormModel;
+            Assert.IsNotNull(model);
+            Assert.That(availableThicknesses.Count, Is.EqualTo(model.AvailableThicknesses.Count));
+            Assert.IsTrue(model.MaterialTypes.Any(mt => mt.Name == "Metal"));
+        }
+
+        [Test] //POST
+        public async Task ActionAddShouldReturnViewWithModelIfModelStateIsInvalid()
+        {
+            var availableThicknesses = new List<double>() { 1, 2, 3 };
+            var types = new List<MaterialTypeViewModel>()
+            {
+                new MaterialTypeViewModel()
+                {
+                    Id = 1,
+                    Name = "Metal"
+                }
+            };
+
+            var materialFormModel = new MaterialFormModel()
+            {
+                SelectedThicknesses = availableThicknesses,
+                MaterialTypeId = 1,
+                CorrosionResistance = false,
+                Density = 3,
+                Description = "Description",
+                ImageUrl = "ImageURL",
+                Name = "Name",
+                PricePerSquareMeter = 120m,
+            };
+
+            _thicknessServiceMock.Setup(ts => ts.ThicknessesAreValidAsync(availableThicknesses)).ReturnsAsync(false);
+            _materialServiceMock.Setup(ms => ms.MaterialTypeExistsAsync(1)).ReturnsAsync(false);
+
+            _materialController = new MaterialController(_materialServiceMock.Object, _thicknessServiceMock.Object);
+
+            var result = await _materialController.Add(materialFormModel);
+
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+        }
+
+        [Test] //POST
+        public async Task ActionAddShouldRedirectToActionWhenModelStateIsValid()
+        {
+            var availableThicknesses = new List<double>() { 1, 2, 3 };
+            var types = new List<MaterialTypeViewModel>()
+            {
+                new MaterialTypeViewModel()
+                {
+                    Id = 1,
+                    Name = "Metal"
+                }
+            };
+
+            var materialFormModel = new MaterialFormModel()
+            {
+                SelectedThicknesses = availableThicknesses,
+                MaterialTypeId = 1,
+                CorrosionResistance = false,
+                Density = 3,
+                Description = "Description",
+                ImageUrl = "ImageURL",
+                Name = "Name",
+                PricePerSquareMeter = 120m,
+            };
+
+            _thicknessServiceMock.Setup(ts => ts.ThicknessesAreValidAsync(availableThicknesses)).ReturnsAsync(true);
+            _materialServiceMock.Setup(ms => ms.MaterialTypeExistsAsync(1)).ReturnsAsync(true);
+
+            _materialController = new MaterialController(_materialServiceMock.Object, _thicknessServiceMock.Object);
+
+            var result = await _materialController.Add(materialFormModel);
+
+            Assert.IsInstanceOf(typeof(RedirectToActionResult), result);
+
+            var resultRedirect = result as RedirectToActionResult;
+
+            string expectedActionName = "All";
+            string expectedControllerName = "Material";
+            string expectedAreaName = "Admin";
+
+            Assert.IsNotNull(resultRedirect);
+            Assert.That(expectedActionName, Is.EqualTo(resultRedirect.ActionName));
+            Assert.That(expectedControllerName, Is.EqualTo(resultRedirect.ControllerName));
+
+            Assert.IsNotNull(resultRedirect.RouteValues);
+            Assert.IsTrue(resultRedirect.RouteValues.Any(v => v.Value?.ToString() == expectedAreaName));
+        }
+
+        [Test] //GET
+        public async Task ActionEditShouldReturnBadRequestForNonExistingMaterial()
+        {
+            var result = await _materialController.Edit(1);
+
+            Assert.IsAssignableFrom<BadRequestResult>(result);
+        }
+
+        [Test]
+        public async Task ActionEditShouldReturnViewResultWithModelWhenModelStateIsInvalid()
+        {
+            var materialModel = new MaterialFormModel()
+            {
+                MaterialTypeId = 1,
+                CorrosionResistance = false,
+                PricePerSquareMeter = 12,
+                Density = 4,
+                Description = "DEscription",
+                Name = "Name",
+                ImageUrl = "ImageURL",
+            };
+
+            _materialServiceMock.Setup(ms => ms.MaterialTypeExistsAsync(materialModel.MaterialTypeId)).ReturnsAsync(false);
+            _thicknessServiceMock.Setup(ts => ts.ThicknessesAreValidAsync(new List<double>() { 1, 2 })).ReturnsAsync(false);
+
+            _materialController = new MaterialController(_materialServiceMock.Object, _thicknessServiceMock.Object);
+
+            var result = await _materialController.Edit(1, materialModel);
+
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+
+            var resultView = result as ViewResult;
+            Assert.IsNotNull(resultView);
+
+            var resultModel = resultView.ViewData.Model as MaterialFormModel;
+            Assert.IsNotNull(resultModel);
+
+            Assert.IsTrue(resultModel.Name == materialModel.Name);
+            Assert.IsTrue(resultModel.Description == materialModel.Description);
+            Assert.IsTrue(resultModel.PricePerSquareMeter == materialModel.PricePerSquareMeter);
+        }
+    }
+}
---
+++ HelixLaserWorks.Tests/ControllersTests/MaterialControllerTest.cs
@@ -0,0 +1,134 @@
+﻿using HelixLaserWorks.Controllers;
+using HelixLaserWorks.Core.Contracts;
+using HelixLaserWorks.Core.Models.Material;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace HelixLaserWorks.Tests.ControllersTests
+{
+    [TestFixture]
+    public class MaterialControllerTest
+    {
+        private Mock<IMaterialService> _materialServiceMock;
+        private MaterialController _materialController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _materialServiceMock = new Mock<IMaterialService>();
+            _materialController = new MaterialController(_materialServiceMock.Object);
+        }
+
+        [Test]
+        public async Task ActionAllReturnsViewResultWithCorrectModel()
+        {
+            IEnumerable<MaterialViewModel> materials = new List<MaterialViewModel>() {
+                new MaterialViewModel()
+                {
+                    Id = 1,
+                    Name = "Name",
+                    Description = "Description",
+                    ImageUrl = "imageURl",
+                    IsAvailable = true,
+                    Type = "Type"
+                }
+            };
+
+            _materialServiceMock = new Mock<IMaterialService>();
+
+            _materialServiceMock.Setup(ms => ms.AllAvailableAsync())
+                .ReturnsAsync(materials);
+
+            _materialController = new MaterialController(_materialServiceMock.Object);
+
+            var result = await _materialController.All();
+
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+
+            var viewModel = result as ViewResult;
+
+            Assert.IsNotNull(viewModel);
+            var resultModels = viewModel.ViewData.Model as IEnumerable<MaterialViewModel>;
+
+            Assert.IsNotNull(resultModels);
+            Assert.IsTrue(resultModels.Count() == 1);
+            Assert.IsTrue(resultModels.First().Name == "Name");
+            Assert.IsTrue(resultModels.First().Id == 1);
+        }
+
+        [Test]
+        public async Task ActionDetailsReturnsViewForValidModel()
+        {
+            var validMaterialModel = new MaterialDetailViewModel()
+            {
+                Id = 1,
+                Name = "Name",
+                Description = "Description"
+            };
+
+            _materialServiceMock = new Mock<IMaterialService>();
+
+            _materialServiceMock.Setup(ms => ms.GetDetailsByIdAsync(validMaterialModel.Id))
+                .ReturnsAsync(validMaterialModel);
+
+            _materialController = new MaterialController(_materialServiceMock.Object);
+
+            var result = await _materialController.Details(validMaterialModel.Id);
+
+            Assert.IsInstanceOf(typeof(ViewResult), result);
+
+            var viewModel = result as ViewResult;
+
+            Assert.IsNotNull(viewModel);
+            var resultModel = viewModel.ViewData.Model as MaterialDetailViewModel;
+
+            Assert.IsNotNull(resultModel);
+            Assert.IsTrue(resultModel.Name == validMaterialModel.Name);
+            Assert.IsTrue(resultModel.Id == validMaterialModel.Id);
+            Assert.IsTrue(resultModel.Description == validMaterialModel.Description);
+        }
+
+        [Test]
+        public async Task ActionDetailsReturnsBadRequestWhenMaterialDoesNotExist()
+        {
+            var validMaterialModel = new MaterialDetailViewModel()
+            {
+                Id = 1,
+                Name = "Name",
+                Description = "Description"
+            };
+
+            _materialServiceMock = new Mock<IMaterialService>();
+
+            _materialServiceMock.Setup(ms => ms.GetDetailsByIdAsync(validMaterialModel.Id))
+                .ReturnsAsync(validMaterialModel);
+
+            _materialController = new MaterialController(_materialServiceMock.Object);
+
+            int nonExistingId = 50;
+
+            var result = await _materialController.Details(nonExistingId);
+
+            Assert.IsInstanceOf(typeof(BadRequestResult), result);
+        }
+
+        [Test]
+        public async Task ActionGetAvailableThicknessesJSONShouldReturnJSONObjectWithAllAvailableThicknessesForGivenMaterialId()
+        {
+            var thicknesses = new List<double>() { 1, 2, 3, 12 };
+
+            int materialId = 1;
+
+            _materialServiceMock = new Mock<IMaterialService>();
+
+            _materialServiceMock.Setup(ms => ms.GetAvailableThicknessesForMaterialAsync(materialId))
+                .ReturnsAsync(thicknesses);
+
+            _materialController = new MaterialController(_materialServiceMock.Object);
+
+            var result = await _materialController.GetAvailableThicknessesJSON(materialId);
+
+            Assert.IsInstanceOf(typeof(JsonResult), result);
+        }
+    }
+}
