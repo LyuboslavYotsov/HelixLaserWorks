0bc7fd10ece58adc83bcde40ed6957236382daba
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-04-16 18:12:14 +0300

    New order implementation reworked

---
+++ HelixLaserWorks/wwwroot/js/adminNotification.js
@@ -0,0 +1,26 @@
+﻿
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/notificationHub")
+    .build();
+
+connection.start().then(function () {
+    console.log("SignalR Connected!");
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+connection.on("ReceiveNotification", function (message) {
+
+});
+
+connection.on("ReceiveNotification", function (message) {
+    Toastify({
+        text: message,
+        duration: 60000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+        stopOnFocus: true
+    }).showToast();
+});
